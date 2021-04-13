4bcb2040b25b87dc6cbe7f74f2769f0cf9d8b364
birngarten <resitozyurt@gmail.com>
2021-04-13 15:44:41 +0200

    Lampda

---
+++ src/lambda/Lambda01Kurallar.java
@@ -0,0 +1,23 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Javada Lambda fonksiyonları sadece Fonksiyonel interface'ler ile kullanılabilir.
+// Sadece bir adet abstract metodu olan interface'lere fonksiyonel interface denilir.
+// Kullanıcı isterse kendi fonksiyonel interface'ini yazabilir. Ama Javada hali hazırda
+// Consumer, Function, Supplier, Predicate gibi tanımlanmış interfaceler bulunmaktadır.
+// Ayrıca bu interface'leri parametre olarak alabilen High Order Function (HOF)'lar da bulunmaktadır.
+// foreEach(), map(), filter(), reduce() gibi HOF'lar fonksiyonel interfaceler ile çalıştığı için
+// içerisinde Lambda fonksiyonları yazmak mümkündür.
+// Javada genelde bu gibi fonksiyonlar üzerinden Lambda ifadeleri kullanılmaktadır.
+
+public class Lambda01Kurallar {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+
+        list.forEach(i -> System.out.println(i));
+    }
+
+}
