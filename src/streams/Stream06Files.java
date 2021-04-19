dce94059bd2cfc99e024c03ca0df8e90224a0884
birngarten <resitozyurt@gmail.com>
2021-04-19 16:16:04 +0200

    Lambda File

--- src/streams/Stream06Files.java
+++ src/streams/Stream06Files.java
@@ -1,4 +1,16 @@
 package streams;
 
+import java.io.IOException;
+import java.nio.file.*;
+//import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class Stream06Files {
+
+    public static void main(String[] args) throws IOException {
+
+        Stream<String> satirlar = Files.lines(Path.of("src/streams/mars.txt")); // Path.of()
+        satirlar.forEach(System.out::println);
+
+    }
 }
